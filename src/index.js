ef0d6a014f6a5c63110bd90b5bf2b342556a7198
andrewZavarykin <andrewzavarykin@gmail.com>
2025-09-03 21:40:06 +0300

    add style and css loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './style.css';
+
 function greetings() {
     const element = document.createElement('p');
     element.innerText = 'Hello world!';
+    element.className = 'hello';
     return element;
 }
 

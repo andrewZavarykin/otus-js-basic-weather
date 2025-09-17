9ce493e7b2410cb2f86d6f3044e9c60def160051
andrewZavarykin <andrewzavarykin@gmail.com>
2025-09-17 15:08:46 +0300

    add tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,24 @@
-import { greetings } from "./index.js";
+import { renderH1 } from "./index.js";
+import { renderForm } from "./index.js";
 
-describe("test greetings", () => {
-  const element = document.querySelector("p");
+describe("test render h1", () => {
+  const elH1 = document.querySelector(".header-appName");
   test("is function", () => {
-    const type = typeof greetings;
+    const type = typeof renderH1;
     expect(type).toBe("function");
   });
-  test("create element <p>", () => {
-    expect(element).not.toBeNull();
+  test("header is exists", () => {
+    expect(elH1).not.toBeNull();
   });
-  test("class is hello", () => {
-    expect(element.className).toBe("hello");
+});
+
+describe("test render form", () => {
+  const elForm = document.querySelector(".form-main");
+  test("is function", () => {
+    const type = typeof renderForm;
+    expect(type).toBe("function");
+  });
+  test("form is exists", () => {
+    expect(elForm).not.toBeNull();
   });
 });
